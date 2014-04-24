21445a4d7f85866a442a70a8607c11e515a802bf
batikuling <batikulingcruz@gmail.com>
2014-04-23 17:53:02 -0700

    Made a change to WorldGridConverter
    Made the methods of WorldGridConverter const.

--- world_grid_converter.hpp
+++ world_grid_converter.hpp
@@ -24,16 +24,16 @@ public:
     WorldGridConverter(int x = 32, int y = 32): _tile_size(x, y){}
 
     template <typename T>
-    sf::Vector2<T> world_to_grid(const sf::Vector2<T>& world_position);
+    sf::Vector2<T> world_to_grid(const sf::Vector2<T>& world_position) const;
 
     template <typename T>
-    sf::Vector2<T> world_to_world_topleft(const sf::Vector2<T>& world_position);
+    sf::Vector2<T> world_to_world_topleft(const sf::Vector2<T>& world_position) const;
 
     template <typename T>
-    sf::Vector2<T> grid_to_world_topleft(const sf::Vector2<T>& grid_position);
+    sf::Vector2<T> grid_to_world_topleft(const sf::Vector2<T>& grid_position) const;
 
     template <typename T>
-    sf::Vector2<T> grid_to_world_center(const sf::Vector2<T>& grid_position);
+    sf::Vector2<T> grid_to_world_center(const sf::Vector2<T>& grid_position) const;
 
 private:
     sf::Vector2i _tile_size;
@@ -42,26 +42,26 @@ private:
 
 
 template <typename T>
-sf::Vector2<T> WorldGridConverter::world_to_grid(const sf::Vector2<T>& world_position){
+sf::Vector2<T> WorldGridConverter::world_to_grid(const sf::Vector2<T>& world_position) const{
     return sf::Vector2<T>(int(world_position.x) / _tile_size.x, int(world_position.y) / _tile_size.y);
 }
 
 
 template <typename T>
-sf::Vector2<T> WorldGridConverter::world_to_world_topleft(const sf::Vector2<T>& world_position){
+sf::Vector2<T> WorldGridConverter::world_to_world_topleft(const sf::Vector2<T>& world_position) const{
     auto res = world_to_grid(world_position);
     return grid_to_world_topleft(res);
 }
 
 
 template <typename T>
-sf::Vector2<T> WorldGridConverter::grid_to_world_topleft(const sf::Vector2<T>& grid_position){
+sf::Vector2<T> WorldGridConverter::grid_to_world_topleft(const sf::Vector2<T>& grid_position) const{
     return sf::Vector2<T>(int(grid_position.x) * _tile_size.x, int(grid_position.y) * _tile_size.y);
 }
 
 
 template <typename T>
-sf::Vector2<T> WorldGridConverter::grid_to_world_center(const sf::Vector2<T>& grid_position){
+sf::Vector2<T> WorldGridConverter::grid_to_world_center(const sf::Vector2<T>& grid_position) const{
     auto res = grid_to_world_topleft(grid_position);
     return sf::Vector2<T>(res.x + _tile_size.x / 2, res.y + _tile_size.y / 2);
 }
